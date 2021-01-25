bd6155f01b681bce139e16c7b91d831650285014
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2021-01-25 14:46:46 +0800

    Contour IV

--- line002/image_processing.py
+++ line002/image_processing.py
@@ -34,10 +34,62 @@ def image_processing_1(image_name,image_path):
             mask = np.zeros(gray.shape,dtype=np.uint8)
             cv.drawContours(mask,contours[i],-1,(255,255,255),-1)
             meanVal = cv.mean(img,mask=mask) #mask是一個區域，所以必須是單通道的
+
             if meanVal[2]>meanVal[1] and meanVal[2]>meanVal[1]:
                 print(meanVal)#將輪廓中的平均數值print出來
-                if abs(meanVal[2]-meanVal[1])>20:#若紅色與綠色絕對值差20以上
+                if abs(meanVal[2]-meanVal[1])<20:#若紅色與綠色絕對值差20以上
                     copy = cv.drawContours(img,contours[i],-1,(0,0,255),2)#將輪廓繪製到空白底圖中
+
+                    #外接矩形
+                    copy = img.copy()
+                    x,y,w,h = cv.boundingRect(contours[i])
+                    brcnt = np.array([[[x,y]],[[x+w,y]],[[x+w,y+h]],[[x,y+h]]])
+                    cv.drawContours(copy,[brcnt],-1,(255,0,0),2)
+                    a = './static/a_'+image_name
+                    cv.imwrite(a,copy)
+
+                    #最小外接矩形
+                    copy = img.copy()
+                    rect = cv.minAreaRect(contours[i])
+                    points = cv.boxPoints(rect)
+                    repoint = np.array(points).reshape((-1,1,2)).astype(np.int32)
+                    cv.drawContours(copy,[repoint],0,(255,0,0),2)
+                    b = './static/b_'+image_name
+                    cv.imwrite(b,copy)
+
+                    #最小外接圓形
+                    copy = img.copy()
+                    (x,y) , radius = cv.minEnclosingCircle(contours[i])
+                    center = (int(x),int(y))
+                    radius = int(radius)
+                    cv.circle(copy,center,radius,(255,0,0),2)
+                    c = './static/c_'+image_name
+                    cv.imwrite(c,copy)
+
+                    #最小外接橢圓
+                    copy = img.copy()
+                    ellipse = cv.fitEllipse(contours[i])
+                    cv.ellipse(copy,ellipse,(255,0,0),3)
+                    d = './static/d_'+image_name
+                    cv.imwrite(d,copy)
+
+                    #最小擬合直線
+                    copy = img.copy()
+                    rows,cols = copy.shape[:2]
+                    [vx,vy,x,y] = cv.fitLine(contours[i],cv.DIST_L2,0,0.01,0.01)
+                    lefty = int(((-x*vy/vx)+y))
+                    righty = int(((cols-x)*vy/vx)+y)
+                    cv.line(copy,(cols-1,righty),(0,lefty),(255,0,0),2)
+                    e = './static/e_'+image_name
+                    cv.imwrite(e,copy)
+
+                    #最小外接三角
+                    copy = img.copy()
+                    area,trgl = cv.minEnclosingTriangle(contours[i])
+                    for i in range(0,3):
+                        cv.line(copy,tuple(trgl[i][0]),tuple(trgl[(i+1)%3][0]),(255,0,0),2)
+                    f = './static/f_'+image_name
+                    cv.imwrite(f,copy)
 
     #將複製圖存實體檔案
     contour_image_path = './static/contour.png'
