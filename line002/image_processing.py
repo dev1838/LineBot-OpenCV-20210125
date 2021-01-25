08bd6297fd7f036fac36c82d20a3d6dabea413f9
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2021-01-25 14:42:47 +0800

    Contour II

--- line002/image_processing.py
+++ line002/image_processing.py
@@ -11,16 +11,32 @@ def image_processing_1(image_name,image_path):
     gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
     
     #將灰階圖進行二值化處理
-    ret,binary=cv.threshold(gray,127,255,cv.THRESH_BINARY)
+    ret,binary=cv.threshold(gray,200,255,cv.THRESH_BINARY)
 
     #將二值化的圖片放到
-    contours,hierarchy = cv.findContours(binary,cv.RETR_LIST,cv.CHAIN_APPROX_SIMPLE)
+    contours,hierarchy = cv.findContours(binary,cv.RETR_LIST,cv.CHAIN_APPROX_NONE)  
 
     #首先複製原圖
     copy = img.copy()
 
     #建立空白圖片
     empty = np.ones(img.shape,dtype=np.uint8)*255
+
+    total_area = img.shape[0]*img.shape[1]
+    font = cv.FONT_HERSHEY_DUPLEX #text type
+    n = len(contours)
+    for i in range(n):
+        M = cv.moments(contours[i])
+        area = M['m00']
+        if area/total_area>0.15 and area/total_area<0.50:#若面積大於總面積的15%且小於50%
+            cx = int(M['m10']/M['m00'])
+            cy = int(M['m01']/M['m00'])
+            center = tuple((cx,cy))
+            if cx>img.shape[1]*0.25 and cx<img.shape[1]*0.75:
+                cv.putText(empty,'. no.='+str(i),center,font,2,(0,0,255),1)
+                print('輪廓編號%d，面積大小%d，中心點%s'%(i,area,str(center)))      
+                #將輪廓描繪在複製圖上
+                copy = cv.drawContours(empty,contours[i],-1,(255,0,0),2)
 
     #將輪廓描繪在複製圖上
     # copy = cv.drawContours(copy,contours,-1,(255,0,0),2)
--- line002/views.py
+++ line002/views.py
@@ -12,9 +12,12 @@ from linebot.models import *
 #models.py資料表
 from line002.models import *
 from line002.flex import *
-from line002.flex import *
-from line002.image_process import *
+from line002.image_processing import *
 
+import string
+import random
+import os
+import csv
 
 line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
 parser = WebhookParser(settings.LINE_CHANNEL_SECRET)
@@ -39,45 +42,37 @@ def callback(request):
 
         for event in events:
             if isinstance(event, MessageEvent):
-                #print(event.message.type)
-                message=[]
-                uid=event.source.user_id
-                profile=line_bot_api.get_profile(uid)
-                name=profile.display_name
+                # print(event.message.type)
                 if event.message.type=='text':
-                    mtext = event.message.text
-                    if 'jobs' in mtext:
-                            job = mtext.split(',')
-                            Jobs.objects.create(uid=uid,
-                                                name=name,
-                                                job_name=job[1],
-                                                percentage=job[2],
-                                                description=job[3])
-                            message.append(TextSendMessage(text='收到的工作內容為：'+str(job)))
-                            message.append(TextSendMessage(text='建立工作內容完成'))
-                            line_bot_api.reply_message(event.reply_token,message)
-                    elif "工作查詢" in mtext:
-                        message.append(jobs_progress(uid))
+                    mtest = event.message.text
+                    if "FlexTest" in mtest:
+                        message.append(flexexample())
                         line_bot_api.reply_message(event.reply_token,message)
-                    else:
+                    else:    
                         message.append(TextSendMessage(text='文字訊息'))
                         line_bot_api.reply_message(event.reply_token,message)
-
                 elif event.message.type=='image':
-
+                    image_name = ''.join(random.choice(string.ascii_letters + string.digits) for x in range(4))
                     image_content = line_bot_api.get_message_content(event.message.id)
-                    path='./static/+'tmp.jpg'
+                    image_name = image_name.upper()+'.png'
+                    path='./static/'+image_name
                     with open(path, 'wb') as fd:
                         for chunk in image_content.iter_content():
                             fd.write(chunk)
-                    
+    
                     #將原圖存為灰階、二值化圖片
-                    gray,binary = image_processing_1(image_name,path)
-                    domain = 'eaedbc149726.ngrok.io'
+                    gray,binary,contour = image_processing_1(image_name,path)
+
+                    domain = 'b3c4322ec312.ngrok.io'
+
                     gray = 'https://'+domain+gray[1:]
                     binary = 'https://'+domain+binary[1:]
+                    contour = 'https://'+domain+contour[1:]
+
                     message.append(ImageSendMessage(original_content_url=gray,preview_image_url=gray))
                     message.append(ImageSendMessage(original_content_url=binary,preview_image_url=binary))
+                    message.append(ImageSendMessage(original_content_url=contour,preview_image_url=contour))
+
                     line_bot_api.reply_message(event.reply_token,message)
 
                 elif event.message.type=='location':
