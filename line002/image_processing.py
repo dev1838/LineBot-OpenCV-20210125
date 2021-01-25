48a64a363a5a23aaabfaafb78a01218faccb4fdf
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2021-01-25 14:45:04 +0800

    Contour III

--- line002/image_processing.py
+++ line002/image_processing.py
@@ -11,10 +11,10 @@ def image_processing_1(image_name,image_path):
     gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
     
     #將灰階圖進行二值化處理
-    ret,binary=cv.threshold(gray,200,255,cv.THRESH_BINARY)
+    ret,binary=cv.threshold(gray,220,255,cv.THRESH_BINARY)
 
     #將二值化的圖片放到
-    contours,hierarchy = cv.findContours(binary,cv.RETR_LIST,cv.CHAIN_APPROX_NONE)  
+    contours,hierarchy = cv.findContours(binary,cv.RETR_LIST,cv.CHAIN_APPROX_NONE)
 
     #首先複製原圖
     copy = img.copy()
@@ -23,24 +23,21 @@ def image_processing_1(image_name,image_path):
     empty = np.ones(img.shape,dtype=np.uint8)*255
 
     total_area = img.shape[0]*img.shape[1]
-    font = cv.FONT_HERSHEY_DUPLEX #text type
+    font = cv.FONT_HERSHEY_PLAIN #設定cv字型
+
     n = len(contours)
     for i in range(n):
         M = cv.moments(contours[i])
         area = M['m00']
-        if area/total_area>0.15 and area/total_area<0.50:#若面積大於總面積的15%且小於50%
-            cx = int(M['m10']/M['m00'])
-            cy = int(M['m01']/M['m00'])
-            center = tuple((cx,cy))
-            if cx>img.shape[1]*0.25 and cx<img.shape[1]*0.75:
-                cv.putText(empty,'. no.='+str(i),center,font,2,(0,0,255),1)
-                print('輪廓編號%d，面積大小%d，中心點%s'%(i,area,str(center)))      
-                #將輪廓描繪在複製圖上
-                copy = cv.drawContours(empty,contours[i],-1,(255,0,0),2)
-
-    #將輪廓描繪在複製圖上
-    # copy = cv.drawContours(copy,contours,-1,(255,0,0),2)
-    copy = cv.drawContours(empty,contours,-1,(255,0,0),2)
+        if area/total_area>0.001 and area/total_area<0.800:#若面積大於總面積的15%且小於50%
+            #將輪廓描繪在複製圖上
+            mask = np.zeros(gray.shape,dtype=np.uint8)
+            cv.drawContours(mask,contours[i],-1,(255,255,255),-1)
+            meanVal = cv.mean(img,mask=mask) #mask是一個區域，所以必須是單通道的
+            if meanVal[2]>meanVal[1] and meanVal[2]>meanVal[1]:
+                print(meanVal)#將輪廓中的平均數值print出來
+                if abs(meanVal[2]-meanVal[1])>20:#若紅色與綠色絕對值差20以上
+                    copy = cv.drawContours(img,contours[i],-1,(0,0,255),2)#將輪廓繪製到空白底圖中
 
     #將複製圖存實體檔案
     contour_image_path = './static/contour.png'
